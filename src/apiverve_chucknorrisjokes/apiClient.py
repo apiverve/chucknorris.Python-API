bac1bb8f7c4552a9f8857d2971b573cbd96ce765
APIVerve Team <hello@apiverve.com>
2025-03-28 17:06:01 -0500

    Bug Fixes

--- src/apiverve_chucknorrisjokes/apiClient.py
+++ src/apiverve_chucknorrisjokes/apiClient.py
@@ -10,10 +10,10 @@ class ChucknorrisAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/chucknorris'
+        self.base_url = 'https://api.apiverve.com/v1/chucknorris'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
